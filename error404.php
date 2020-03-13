d79c509da7ad60e17305c8fa5bff9863642f480a
Paul Guillon <alentak27@gmail.com>
2020-03-13 15:45:00 -0400

    erreur 404 custom

---
+++ error404.php
@@ -0,0 +1,15 @@
+<?php include_once 'php/class/all.class.php'; ?>
+<?php include_once 'php/inc/head.inc.php'; ?>
+<title>Kart'Québéc - Error 404</title>
+<?php include_once 'php/inc/header.inc.php'; ?>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Erreur 404 - Page introuvable</h1>
+                <a href="index.php"><button class="btn btn-secondary">Retourner à l'accueil</button></a>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include_once 'php/inc/footer.inc.php'; ?>
